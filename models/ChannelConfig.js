f954547f442fb19247803832902faed007192a21
dareque3n-maker <dareque3n@gmail.com>
2026-08-15 06:33:16 +0530

    Delete models directory

--- models/ChannelConfig.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const channelConfigSchema = new mongoose.Schema({
-    guildId: { type: String, required: true, unique: true },
-    channelId: { type: String, required: true }
-});
-
-module.exports = mongoose.model('ChannelConfig', channelConfigSchema);
